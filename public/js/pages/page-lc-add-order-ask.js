ba276dbb33fc1a9058edcb6d3758179f858f58c2
Vladyslav Dykyi <vladislavdykyi33@gmail.com>
2022-05-05 22:47:42 +0300

    upd

--- public/js/pages/page-lc-add-order-ask.js
+++ public/js/pages/page-lc-add-order-ask.js
@@ -2,12 +2,6 @@ import {bindTabs2} from '../default/module-lc.js';
 
 bindTabs2('#tabs2');
 
-const share_type = 'share_type';
-const volume = 'volume';
-const valuation = 'valuation';
-const share_price = 'share_price';
-const share_type_currency = 'share_type_currency';
-const share_number = 'share_number';
 
 
 let items = document.querySelector('#tabs2');
@@ -16,12 +10,10 @@ let itemsTabs = items.querySelectorAll('.content-t2');
 
 for (let i = 0; i < itemsUl.length; i++) {
 	const titleEl = itemsUl[i];
-
 	titleEl.addEventListener('click', e => {
 		clearName(itemsTabs);
 	});
-
-}
+} 
 
 function clearName(e) {
 	for (let i = 0; i < e.length; i++) {
@@ -33,7 +25,7 @@ function clearName(e) {
 			deleteName(elementInput);
 			deleteName(elementSelect);
 		} else {
-			if (e[i] == e[0]) {
+			if (e[i] === e[0]) {
 				addName2();
 			} else {
 				addName();
@@ -48,12 +40,13 @@ function clearName(e) {
 }
 
 function addName() {
+	debugger;
 	let items1 = document.querySelector('#volume2');
 	let items2 = document.querySelector('#share_number2');
 	let items3 = document.querySelector('#share_type2');
-	items1.setAttribute('name', volume);
-	items2.setAttribute('name', valuation);
-	items3.setAttribute('name', share_type);
+	items1.setAttribute('name', 'volume');
+	items2.setAttribute('name', 'valuation');
+	items3.setAttribute('name', 'share_type');
 }
 
 function addName2() {
@@ -63,10 +56,10 @@ function addName2() {
 	let items4 = document.querySelector('#share_type_currency1');
 	let items5 = document.querySelector('#share_type_currency2');
 	let items6 = document.querySelector('#share_type');
-	items1.setAttribute('name', volume);
-	items2.setAttribute('name', share_number);
-	items3.setAttribute('name', share_price);
-	items4.setAttribute('name', share_type_currency);
-	items5.setAttribute('name', share_type_currency);
-	items6.setAttribute('name', share_type);
+	items1.setAttribute('name', 'volume');
+	items2.setAttribute('name', 'share_number');
+	items3.setAttribute('name', 'share_price');
+	items4.setAttribute('name', 'share_type_currency');
+	items5.setAttribute('name', 'share_type_currency');
+	items6.setAttribute('name', 'share_type');
 }
