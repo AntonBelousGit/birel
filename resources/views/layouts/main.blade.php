15709389d18d5be0273e48358bba6c761dc6fa61
AntonBelousGit <antonbelouswork@gmail.com>
2022-07-02 13:50:13 +0300

    CHAT

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,6 +12,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Fonts -->
+    @yield('meta')
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Styles -->
@@ -19,6 +20,7 @@
 
     <link rel="stylesheet" href="{{ asset('css/default/style-lc.min.css') }}">
     @yield('style')
+    @yield('scripts_head')
 </head>
 
 <body>
